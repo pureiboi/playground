402a887f0dcc051234580e379694e719f2f3fb0c
pureiboi <17396188+pureiboi@users.noreply.github.com>
2026-03-04 01:54:55 +0800

    go code

---
+++ go/cmd/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+import "playground/internal/service"
+
+func main() {
+	fmt.Println("Demo Hello")
+
+	fmt.Println(service.Add(99, 204))
+}
---
+++ go/internal/service/util.go
@@ -0,0 +1,5 @@
+package service
+
+func Add(x int, y int) int {
+	return x + y
+}
